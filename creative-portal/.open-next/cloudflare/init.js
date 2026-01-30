c87e9ff84773fba5974bfee6a645b10785779a50
Luis Gilberto <me@luis-gilberto.com>
2026-01-29 16:51:30 -0800

    Vanilla HTML deploy prep: StrategyIQ mobile card accessibility; Latest Insight signature fix; exclude Portal via .wranglerignore

--- creative-portal/.open-next/cloudflare/init.js
+++ creative-portal/.open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1769151926582,
+    __BUILD_TIMESTAMP_MS__: 1769653644569,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
