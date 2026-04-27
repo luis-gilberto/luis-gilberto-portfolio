42eb72da10b9b83d44406254c62bb587a0d72f5d
Luis Gilberto <me@luis-gilberto.com>
2026-04-27 08:25:54 -0700

    fix(nari): add _redirects to nari client folder and update root redirects to prevent interception

--- creative-portal/.open-next/cloudflare/init.js
+++ creative-portal/.open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1777127018662,
+    __BUILD_TIMESTAMP_MS__: 1777197241667,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
